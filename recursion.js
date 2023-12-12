2a15770a76f48eaf9c8efbe62de68f8ebc254b23
Euan <euanconlon46@gmail.com>
2023-12-12 14:27:32 +0000

    Recursive Factorial Function

--- recursion.js
+++ recursion.js
@@ -23,3 +23,11 @@ function sum(n) {
 for (let i = 1; i <= 10; i++) {
     console.log(sum(i));
 }
+
+// recursive factorial function
+function factorial(n) {
+    if (n === 0) 
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
