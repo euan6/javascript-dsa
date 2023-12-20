5d41f45f5b53a0a41487f5467195738729e873ce
Euan <euanconlon46@gmail.com>
2023-12-20 16:14:06 +0000

    Tests For Queue Implementation

--- queue.js
+++ queue.js
@@ -4,27 +4,27 @@ class Queue {
     constructor(capacity = 1000) {
         this.capacity = capacity;
         this.Q = new Array(capacity);
-        front = 0;
-        rear = 0;
+        this.f = 0;
+        this.r = 0;
     }
 
     // returns a boolean whether queue is empty or not
     isEmpty() {
-        return f === r;
+        return this.f === this.r;
     }
 
     // returns the size of the queue
     size() {
-        return (this.capacity - f + r) % this.capacity;
+        return (this.capacity + this.r - this.f) % this.capacity;
     }
 
     // adds an element to the queue
     enqueue(element) {
-        if (this.size() === this.capacity - 1) {
+        if (this.size() === this.capacity) {
             throw new Error("Queue is Full");
         } else {
-            this.Q[r] = element;
-            r = (r + 1) & this.capacity;
+            this.Q[this.r] = element;
+            this.r = (this.r + 1) % this.capacity;
         }
     }
 
@@ -33,9 +33,9 @@ class Queue {
         if (this.isEmpty()) {
             throw new Error("Queue is Empty");
         }
-        let temp = this.Q[f];
-        this.Q[f] = null;
-        f = (f + 1) & capacity;
+        let temp = this.Q[this.f];
+        this.Q[this.f] = null;
+        this.f = (this.f + 1) % this.capacity;
         return temp;
     }
 
@@ -44,6 +44,21 @@ class Queue {
         if (this.isEmpty()) {
             throw new Error("Queue is Empty");
         }
-        return this.Q[f];
+        return this.Q[this.f];
     }
+}
+
+try {
+    const q = new Queue(5);
+    console.log(q.isEmpty());
+    q.enqueue(5);
+    q.enqueue(8);
+    q.enqueue(2);
+    console.log(q.size());
+    console.log(q.dequeue());
+    console.log(q.size());
+    console.log(q.dequeue());
+} catch (err) {
+    // catches any exceptions
+    console.error("Error: ", err.message);
 }
