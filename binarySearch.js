bca4ee5250c9887e0be2f9a31e336e57e03f3238
Euan <euanconlon46@gmail.com>
2023-12-21 15:40:03 +0000

    Tests For Binary Search Implementation

--- binarySearch.js
+++ binarySearch.js
@@ -12,3 +12,7 @@ function binarySearch(arr, target, first, last) {
         return binarySearch(arr, target, first, mid - 1);
     }
 }
+// displays the results when searching for 5 and 11
+numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(binarySearch(numbersArr, 5, 0, numbersArr.length - 1));
+console.log(binarySearch(numbersArr, 11, 0, numbersArr.length - 1));
