1593e85d536b468c14ffb0c7fe1a0ea4738993b8
Euan <euanconlon46@gmail.com>
2023-12-22 15:40:27 +0000

    Bubble Sort Descending Implemented

--- Algorithms/bubbleSort.js
+++ Algorithms/bubbleSort.js
@@ -17,3 +17,20 @@ function bubbleSortAscending(arr) {
 // displays the sorted array
 arrayAsc = [5, 7, 2, 4, 1, 9];
 console.log(bubbleSortAscending(arrayAsc));
+
+// bubble sort of an array in descending order
+function bubbleSortDescending(arr) {
+    let swaps = true;
+    while (swaps) {
+        swaps = false;
+        for (let i = 0; i < arr.length - 1; i ++) {
+            if (arr[i + 1] > arr[i]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swaps = true;
+            }
+        }
+    }
+    return arr;
+}
