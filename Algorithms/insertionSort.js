fdf1c5c2544c80766033c270df0810643f7ccc85
Euan <euanconlon46@gmail.com>
2023-12-26 23:11:06 +0000

    Insertion Sort Ascending Implemented

--- Algorithms/insertionSort.js
+++ Algorithms/insertionSort.js
@@ -1,4 +1,12 @@
 
-function insertionSort() {
-
+function insertionSortAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j--];
+            arr[j + 1] = current;
+        }
+    }
+    return arr;
 }
