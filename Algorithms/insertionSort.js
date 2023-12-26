c2b70d095c31f26e3832a0cf468d7fc8a56b000a
Euan <euanconlon46@gmail.com>
2023-12-26 23:20:15 +0000

    Insertion Sort Descending Implementation

--- Algorithms/insertionSort.js
+++ Algorithms/insertionSort.js
@@ -13,3 +13,20 @@ function insertionSortAscending(arr) {
 // displays the sorted array
 arrayAsc = [5, 7, 2, 4, 1, 9];
 console.log(insertionSortAscending(arrayAsc));
+
+// insertion sort of an array in descending order
+function insertionSortDescending(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] < current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+// displays the sorted array
+arrayDesc = [5, 7, 2, 4, 1, 9];
+console.log(insertionSortDescending(arrayDesc));
