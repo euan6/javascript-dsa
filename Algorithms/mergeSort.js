76a7a748779ea462c87bb9a3e996883196081c52
Euan <euanconlon46@gmail.com>
2023-12-29 22:36:45 +0000

    Merge Sort Ascending Implementation

--- Algorithms/mergeSort.js
+++ Algorithms/mergeSort.js
@@ -1,4 +1,49 @@
 
-function mergeSortAscending() {
+function mergeSortAscending(arr) {
+    let arrLength = arr.length;
+    if (arrLength < 2) {
+        return arr;
+    } else {
+        let halfSize = Math.floor(arrLength / 2);
+        let left = [];
+        let right = [];
+        let index = 0;
+        while (index < halfSize) {
+            left[index] = arr[index];
+            index++;
+        }
+        index = 0;
+        while (index < arrLength) {
+            right[index - halfSize] = arr[index];
+            index++;
+        }
+        left = mergeSortAscending(left);
+        right = mergeSortAscending(right);
+        merge(left, right, arr);
+    }
+    return arr;
+}
 
+function mergeAscending(left, right, arr) {
+    let index = 0, leftIndex = 0, rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] <= right[rightIndex]) {
+            arr[index] = left[leftIndex];
+            leftIndex++;
+        } else {
+            arr[index] = right[rightIndex];
+            rightIndex++;
+        }
+        index++;
+    }
+    while (leftIndex < left.length) {
+        arr[index] = left[leftIndex];
+        leftIndex++;
+        index++;
+    }
+    while (rightIndex < right.length) {
+        arr[index] = right[rightIndex];
+        rightIndex++;
+        index++;
+    }
 }
