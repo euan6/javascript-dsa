957c1401eaa6c87608f155fc3946fe9045f27746
Euan <euanconlon46@gmail.com>
2024-01-11 18:09:43 +0000

    BSTNode Class implementation Finished

--- Algorithms/binarySearchTree.js
+++ Algorithms/binarySearchTree.js
@@ -26,6 +26,86 @@ class BSTNode {
         return this.value;
     }
 
+    insert(node) {
+        if (node.value < this.value) {
+            if (this.getLeftChild() !== null) {
+                this.getLeftChild().insert(node);
+            } else {
+                this.setLeftChild(node);
+            }
+        } else if (node.value > this.value) {
+            if (this.getRightChild() !== null) {
+                this.getRightChild().insert(node);
+            } else {
+                this.setRightChild(node);
+            }
+        } else {
+            return;
+        }
+    }
+
+    search(value) {
+        if (value < this.value) {
+            if (this.getLeftChild() !== null) {
+                return this.getLeftChild().search(value);
+            } else {
+                return false;
+            }
+        } else if (v > this.value) {
+            if (this.getRightChild() !== null) {
+                return this.getRightChild().search(value);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    delete(value) {
+        if (value !== this.value) {
+            if (v < this.value && this.getLeftChild() !== null) {
+                this.leftChild = this.getLeftChild().delete(value);
+                return this;
+            } else if (v > this.value && this.getRightChild() !== null) {
+                this.rightChild = this.getRightChild().delete(value);
+                return this;
+            } else {
+                return this;
+            }
+        } else {
+            if (this.getLeftChild() !== null && this.getRightChild() !== null) {
+                let largestLeft = this.getLeftChild().getLargestValueNode();
+                this.value = largestLeft.getValue();
+                this.leftChild = this.getLeftChild().delete(largestLeft.getValue());
+                return this;
+            } else if (this.getLeftChild !== null) {
+                return this.getLeftChild();
+            } else if (this.getRightChild() !== null) {
+                return this.getRightChild();
+            } else {
+                return null;
+            }
+        }
+    }
+
+    getLargestValueNode() {
+        if (this.getRightChild() === null) {
+            return this;
+        } else {
+            return this.getRightChild().getLargestValueNode();
+        }
+    }
+
+    numberOfNodes() {
+        let valueToReturn = 1;
+        if (this.leftChild !== null) {
+            valueToReturn += this.leftChild.numberOfNodes();
+        }
+        if (this.rightChild !== null) {
+            valueToReturn += this.rightChild.numberOfNodes();
+        }
+        return valueToReturn;
+    }
 }
 
 class BinarySearchTree {
