8e430b0cc0945dda1b09fc98ca0a62afea4b27a2
Euan <euanconlon46@gmail.com>
2024-01-11 19:50:54 +0000

    Comments Added For Binary Search Tree

--- Algorithms/binarySearchTree.js
+++ Algorithms/binarySearchTree.js
@@ -1,31 +1,38 @@
 class BSTNode {
 
+    // constructor for node with a value
     constructor(value) {
         this.value = value;
         this.leftChild = null;
         this.rightChild = null;
     }
 
+    // returns the left child node
     getLeftChild() {
         return this.leftChild;
     }
 
+    // returns the right child node
     getRightChild() {
         return this.rightChild;
     }
 
+    // sets the left child node
     setLeftChild(node) {
         this.leftChild = node;
     }
 
+    // sets the right child node
     setRightChild(node) {
         this.rightChild = node;
     }
 
+    // returns the value of a node
     getValue() {
         return this.value;
     }
 
+    // inserts a node into the tree
     insert(node) {
         if (node.value < this.value) {
             if (this.getLeftChild() !== null) {
@@ -44,6 +51,7 @@ class BSTNode {
         }
     }
 
+    // searches for a node in the tree
     search(value) {
         if (value < this.value) {
             if (this.getLeftChild() !== null) {
@@ -61,6 +69,7 @@ class BSTNode {
         return true;
     }
 
+    // deletes a node in the tree
     delete(value) {
         if (value !== this.value) {
             if (value < this.value && this.getLeftChild() !== null) {
@@ -88,6 +97,7 @@ class BSTNode {
         }
     }
 
+    // returns the largest value node
     getLargestValueNode() {
         if (this.getRightChild() === null) {
             return this;
@@ -96,6 +106,7 @@ class BSTNode {
         }
     }
 
+    // returns the number of nodes
     numberOfNodes() {
         let valueToReturn = 1;
         if (this.leftChild !== null) {
@@ -110,18 +121,22 @@ class BSTNode {
 
 class BinarySearchTree {
 
+    // constructor for an empty tree
     constructor() {
         this.rootNode = null;
     }
 
+    // returns a boolean whether tree is empty or not
     isEmpty() {
         return this.rootNode === null;
     }
 
+    // returns the size of the binary search tree
     size() {
         return this.rootNode ? this.rootNode.numberOfNodes() : 0;
     }
 
+    // creates new node and inserts into the tree
     insert(value) {
         if (this.rootNode === null) {
             this.rootNode = new BSTNode(value);
@@ -130,12 +145,14 @@ class BinarySearchTree {
         }
     }
 
+    // deletes the node with the input value
     delete(value) {
         if (this.rootNode !== null) {
             this.rootNode = this.rootNode.delete(value);
         }
     }
 
+    // searches for and returns the node with the input value
     search(value) {
         if (this.rootNode !== null) {
             return this.rootNode.search(value);
@@ -146,17 +163,27 @@ class BinarySearchTree {
 }
 
 try {
+    // creates a new binary search tree
     const bst = new BinarySearchTree();
+    // check if the tree is empty
     console.log(bst.isEmpty());
+    // inserts 3 nodes in the tree
     bst.insert(8);
     bst.insert(3);
     bst.insert(7);
+    // check if the tree is empty
     console.log(bst.isEmpty());
+    // displays the size
     console.log(bst.size());
+    // searches for 8 in the tree
     console.log(bst.search(8));
+    // searches for 4 in the tree
     console.log(bst.search(4));
+    // deletes 8 from the tree
     bst.delete(8);
+    // displays the size
     console.log(bst.size());
 } catch (err) {
+    // catches any exceptions
     console.log("Error: ", err.message);
 }
