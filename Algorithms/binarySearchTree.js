b98062148b48f4ec566ae3244ca29a56df5090c5
Euan <euanconlon46@gmail.com>
2024-01-11 17:15:40 +0000

    BSTNode Class Implementation

--- Algorithms/binarySearchTree.js
+++ Algorithms/binarySearchTree.js
@@ -1,3 +1,29 @@
 class BSTNode {
 
+    constructor(value) {
+        this.value = value;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    getLeftChild() {
+        return this.leftChild;
+    }
+
+    getRightChild() {
+        return this.rightChild;
+    }
+
+    setLeftChild(node) {
+        this.leftChild = node;
+    }
+
+    setRightChild(node) {
+        this.rightChild = node;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
 }
